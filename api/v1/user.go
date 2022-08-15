03d7606c1575af088bc11a61d8d00152008ae30d
Shi Jiajun <1178972961@qq.com>
2022-08-15 19:03:59 +0800

    8.15 完成 User Model / API 部分编写

--- api/v1/user.go
+++ api/v1/user.go
@@ -1,28 +1,66 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"go-vue-blog/model"
+	"go-vue-blog/utils/errmsg"
+	"net/http"
+	"strconv"
+)
 
-// 查询用户是否存在（反重名）
-func UserExist(c *gin.Context) {
-
-}
-
-// 添加用户
+// AddUser 添加用户
 func AddUser(c *gin.Context) {
-	//todo 添加用户
+	var user model.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		fmt.Println("AddUser() 绑定 JSON 出错：", err)
+	}
+	//检查重名
+	code := model.CheckUser(user.Username)
+	if code == errmsg.SUCCESS {
+		model.CreateUser(&user)
+	}
+
+	//返回状态:回复http.StatusOK是为了说明 web 服务正常执行了
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    user,
+		"message": errmsg.GetErrMsg(code),
+	})
+
 }
 
 //查询单个用户（在 Blog 系统中用处不大）
 
-// 查询用户列表
+// GetUsers 查询用户列表
 func GetUsers(c *gin.Context) {
+	//从请求报文的 params 提取数据
+	pageSize, _ := strconv.Atoi(c.Query("pagesize"))
+	pageNum, _ := strconv.Atoi(c.Query("pagenum"))
 
+	if pageSize == 0 {
+		pageSize = -1 //GORM 的Limit(-1) 表示不要 Limit() 这个限制
+	}
+	if pageNum == 0 {
+		pageNum = 1 //GORM 的Offset((1-1)*pageSize) 表示不要 Offset() 这个限制
+	}
+
+	//查询数据库
+	users, total := model.GetUsers(pageSize, pageNum)
+
+	//返回数据
+	c.JSON(http.StatusOK, gin.H{
+		"status":  errmsg.SUCCESS,
+		"data":    users,
+		"total":   total,
+		"message": errmsg.GetErrMsg(errmsg.SUCCESS),
+	})
 }
 
-// 编辑用户
+// EditUser 编辑用户
 func EditUser(c *gin.Context) {
 
 }
 
-// 删除用户
+// DeleteUser 删除用户
 func DeleteUser(c *gin.Context) {}
--- model/User.go
+++ model/User.go
@@ -1,10 +1,63 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"go-vue-blog/utils/errmsg"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
 	Username string `gorm:"type:varchar(20);not null" json:"username"`
 	Password string `gorm:"type:varchar(20);not null" json:"password"`
 	Role     int    `gorm:"type:int" json:"role"`
+}
+
+//model 层的函数签名基本和 Controller 层一致。分工如下：
+//		Controller 和前端交互 JSON 数据和请求
+//		Model 根据 Controller 所得的 JSON 数据和请求对数据库进行操作
+
+// CheckUser 检查用户是否存在
+// Params user *User	一个填充了前端 JSON 数据的 User 对象
+// Return code int		一个状态码，表示正确或错误
+func CheckUser(name string) int {
+	var u User //作用：1.规定从哪个表查询  2.将查询结果灌注到该对象中
+	db.
+		Select("id").
+		Where("username = ?", name).
+		First(&u) //字段名全部是小写
+	if u.ID > 0 {
+		return errmsg.ERROR_USERNAME_USED
+	}
+	return errmsg.SUCCESS
+
+}
+
+// CreateUser 向数据库添加用户
+// Params user *User	一个填充了前端 JSON 数据的 User 对象
+// Return code int		一个状态码，表示正确或错误
+func CreateUser(user *User) int { // Go 的 struct 是引用类型，作为参数传入时必须作为一个指针
+	err := db.Create(&user).Error
+	if err != nil {
+		fmt.Println("添加 user 失败： ", err)
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
+
+}
+
+func GetUsers(pageSize int, pageNum int) ([]User, int64) {
+	var users []User
+	var total int64
+	err := db.
+		Find(&users).
+		Count(&total).
+		Limit(pageSize).
+		Offset((pageNum - 1) * pageSize).Error
+	if err != nil {
+		fmt.Println("查找用户列表失败： ", err)
+		return nil, 0
+	}
+	return users, total
+
 }
