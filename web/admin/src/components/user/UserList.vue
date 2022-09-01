74152ae0702164956df15bf0c7eda937c9868bd5
Shi Jiajun <1178972961@qq.com>
2022-09-01 21:55:43 +0800

    9.1 艰难的antdv Table 组件开始了征途

--- web/admin/src/components/user/UserList.vue
+++ web/admin/src/components/user/UserList.vue
@@ -14,24 +14,128 @@
         </a-col>
       </a-row>
 
-      <a-table></a-table>
+      <a-table
+        :dataSource="userList"
+        :columns="columns"
+        :pagination="paginationOption"
+        bordered
+        rowKey="ID"
+      >
+        <!--希望将 role的1、2换成管理员、订阅者 -->
+        <span
+          slot="role"
+          slot-scope="data"
+        >{{data==1?'管理员':'订阅者'}}</span>
+      </a-table>
     </a-card>
   </div>
 </template>
     
-    <script>
+<script>
+const columns = [
+  {
+    title: "ID",
+    dataIndex: "ID", //接收来自json的数据，和json字段一致
+    sorter: true,
+    width: "10%",
+    align: "center",
+    key: "id",
+
+    scopedSlots: { customRender: "id" },
+  },
+  {
+    title: "用户名",
+    dataIndex: "username",
+    align: "center",
+    width: "20%",
+    key: "username",
+  },
+  {
+    title: "角色",
+    dataIndex: "role",
+    width: "20%",
+    align: "center",
+    key: "role",
+    scopedSlots: { customRender: "role" },
+  },
+  {
+    title: "操作",
+    width: "20%",
+    align: "center",
+    key: "action",
+  },
+];
+
 export default {
   name: "AdminIndex",
 
   data() {
-    return {};
+    return {
+      //用户列表的表单数据
+      userList: [],
+
+      //规定列格式
+      columns,
+
+      //分页器配置
+      paginationOption: {
+        pageSizeOptions: ["5", "10", "15", "20"], //指定每页可以显示多少条
+
+        defaultCurrent: 1, //默认的当前页数
+        defaultPageSize: 5, ///默认的每页条数
+
+        total: 0, //数据总量
+        showSizeChanger: true, //允许更改每页条数
+        showTotal: (total, range) => {
+          //拿到总数，控制分页器的描述字符串具体内容
+          //range[0]：当前页码的第一条数据的序号
+          //range[1]：当前页码的最后一条的序号
+          `${range[0]}-${range[1]}  共 ${total} 条数据`;
+        },
+
+        //分页器的时间直接写到这里时，函数名前要加'on'
+        onChange: (page, pageSize) => {
+          //页码改变的回调
+          this.paginationOption.defaultCurrent = page;
+          this.paginationOption.defaultPageSize = pageSize;
+        },
+        // onShowSizeChange: (current, size) => {
+        //   //pageSize变化的回调
+        //   //current: 当前页数
+        //   //size:当前尺寸
+        //   //todo 确定是这两个default 而不是current/pageSize 吗？
+        //   this.paginationOption.defaultCurrent = current;
+        //   this.paginationOption.defaultPageSize = size;
+        // },
+      },
+    };
   },
 
-  mounted() {},
+  created() {
+    //在页面创建前就获取相应数据
+    this.getUserList();
+  },
 
-  methods: {},
+  methods: {
+    async getUserList() {
+      //将旧res的data部分又赋给res.即只保留 res 的 data 部分
+      const { data: res } = await this.$axios.get("users", {
+        params: {
+          pagenum: this.paginationOption.defaultCurrent,
+          pagesize: this.paginationOption.defaultPageSize,
+        },
+      });
+
+      if (res.status != 200) return this.$message.error(res.message);
+
+      //确认无误，赋值
+      this.userList = res.data;
+      this.paginationOption.total = res.total;
+    },
+    onSearch() {},
+  },
 };
 </script>
     
-    <style scoped>
+<style scoped>
 </style>
