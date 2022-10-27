ff1efe5f5211417be49ebf04501381dfcc1a93a4
Shi Jiajun <1178972961@qq.com>
2022-10-27 08:09:31 +0800

    添加 Pinia ，完成需要组件间传值的功能
    1.使用到了Pinia
    2.使用@vue/composition-api ,
    解释：
        之所以在 Vue 2 引入组合式 API 而不是直接使用 Vue 3 ，是因为我的主要方向是后端，目前只想在现有项目前端部分的基础上添加几个新功能。如将来有需要，完整的 Vue 3 技能点将和以后的 Typescript 、Vite 等一起学习。

--- web/front/src/main.js
+++ web/front/src/main.js
@@ -1,14 +1,18 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { createPinia, PiniaVuePlugin } from "pinia";
 import vuetify from "./plugins/vuetify";
-
 import "./plugins/axios";
 
 Vue.config.productionTip = false;
 
+Vue.use(PiniaVuePlugin);
+const pinia = createPinia();
+
 new Vue({
   router,
+  pinia,
   vuetify,
   render: h => h(App),
 }).$mount("#app");
